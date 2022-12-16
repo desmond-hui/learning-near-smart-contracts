af894002740f183e931b44f30fccd512e554aec9
altanapps <futureisaltan@gmail.com>
2022-12-16 14:48:33 +0000

    near-marketplace-contract completed

---
+++ near-course-marketplace-dapp/assembly/index.ts
@@ -0,0 +1,11 @@
+import { PersistentUnorderedMap } from "near-sdk-as";
+
+export const courses = new PersistentUnorderedMap<string,string>("COURSES");
+
+export function addCourse(id: string, name: string): void {
+  courses.set(id, name);
+}
+
+export function getCourse(id: string): string | null {
+  return courses.get(id);
+}
