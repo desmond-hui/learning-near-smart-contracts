9f4f057f4b602a06ea3d71ea3b763209ee921197
altanapps <futureisaltan@gmail.com>
2023-01-25 09:11:29 +0000

    wrote the barebone structure of the contract

--- factory-contract-rust/contract/src/lib.rs
+++ factory-contract-rust/contract/src/lib.rs
@@ -1,10 +1,13 @@
 // Find all our documentation at https://docs.near.org
 use near_sdl::collections::LazyOption;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::{log, near_bindgen, Balance, Gas};
 
-// Convert 
 const CONTRACT_ADDRESS: &[u8] = include_bytes!("./guest-book-contract/contract.wasm");
+const NEAR_PER_STORAGE: Balance = 10_000_000_000_000_000_000; // 10e18yⓃ
+const TGAS: Gas = Gas(10u64.power(12)); // // 10e12yⓃ
+const NO_DEPOSIT: Balance = 0;
+
 
 // Define the contract structure
 #[near_bindgen]
